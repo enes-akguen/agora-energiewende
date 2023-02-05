e854e4ae1c2e2517f3ff36e0717b12f266533a45
Enes Akgün <enesakguen@outlook.de>
2023-02-05 22:21:59 +0100

    [TASK] Improve regex for parsing

--- Enes Akguen Agora Energiewende/src/controller/MainController.java
+++ Enes Akguen Agora Energiewende/src/controller/MainController.java
@@ -12,6 +12,59 @@ public class MainController {
 	 */
 	public static void main(String[] args) {
 		Api api = new Api();
-		Parser.parseEnergyData(api.getJson());
+		
+		
+		/**
+		 * Array containing the start and end range of energy sources for regex pattern
+		 * based on API data structure
+		 */
+		final String[][] energySourcesRange = {
+//				// Konv. Kraftwerke
+//				{"\\\"name\\\":\\\"Konv. Kraftwerke\\\""
+//						,"]},{\\\"id\\\":\\\"emission-co2\\"},
+				// Wasserkraft
+				{"\\\"name\\\":\\\"Wasserkraft\\\""}
+//				// Braunkohle
+//				{"\\\"name\\\":\\\"Braunkohle\\\""
+//					,"]},{\\\"id\\\":\\\"uranium\\\""},
+//				// Andere
+//				{"\\\"name\\\":\\\"Andere\\\""
+//					,"]},{\\\"id\\\":\\\"total-load\\\""},
+//				// Solar
+//				{"\\\"name\\\":\\\"Solar\\\""
+//					,"]},{\\\"id\\\":\\\"wind-onshore\\\""},
+//				// Biomasse
+//				{"\\\"name\\\":\\\"Biomasse\\\""
+//					,"]},{\\\"id\\\":\\\"hydro-pumped-storage\\\""},
+//				// Kernenergie
+//				{"\\\"name\\\":\\\"Kernenergie\\\""
+//					,"]},{\\\"id\\\":\\\"other\\\""},
+//				// CO2-Emissionsfaktor des Strommix
+//				{"\\\"name\\\":\\\"CO<sub>2<\\\\\\/sub>-Emissionsfaktor des Strommix\\"
+//					,"]}]}"},
+//				// Wind Onshore
+//				{"\\\"name\\\":\\\"Wind Onshore\\\""
+//					,"]},{\\\"id\\\":\\\"wind-offshore\\\""},
+//				// Stromverbrauch
+//				{"\\\"name\\\":\\\"Stromverbrauch\\\""
+//					,"]},{\\\"id\\\":\\\"conventional-power\\\""},
+//				// Pumpspeicher
+//				{"\\\"name\\\":\\\"Pumpspeicher\\\""
+//					,"]},{\\\"id\\\":\\\"gas\\\""},
+//				// Wind Offshore
+//				{"\\\"name\\\":\\\"Wind Offshore\\\""
+//					,"]},{\\\"id\\\":\\\"run-of-the-river\\\""},
+//				// Steinkohle
+//				{"\\\"name\\\":\\\"Steinkohle\\\""
+//					,"]},{\\\"id\\\":\\\"run-of-the-river\\\""},
+				// Erdgas
+//				{"\\\"name\\\":\\\"Erdgas\\\""
+//					,"]},{\\\"id\\\":\\\"coal\\\""}
+		};
+		
+		
+		for (int i = 0; i < energySourcesRange.length; i++) {
+			  Parser.parseEnergyData(api.getJson(), energySourcesRange[i][0]);
+		}
 	}
 }
--- Enes Akguen Agora Energiewende/src/service/Parser.java
+++ Enes Akguen Agora Energiewende/src/service/Parser.java
@@ -8,31 +8,34 @@ import java.util.regex.Pattern;
 import utils.Utilities;
 
 public class Parser {
-	public static String parseEnergyData(String jsonString)
+	public static String parseEnergyData(String jsonString, String startIndex)
 	{
-		String wasserKraftData = jsonString.substring(jsonString.indexOf("\\\"Wasserkraft\\\",\\\"legendIndex\\\":40,\\\"index\\\":80,\\\"type\\\":\\\"area\\\",\\\"color\\\":\\\"rgb(121,168,202)\\\",\\\"yAxis\\\":0,\\\"data\\\":")+119, 
-				jsonString.indexOf("]},{\\\"id\\\":\\\"biomass\\"));
-		// System.out.print(data);
-		
-		List<String> wasserKraftDatasets = new ArrayList<String>();
-		Pattern pattern = Pattern.compile("\\[[\\d,\\.]*\\]", Pattern.CASE_INSENSITIVE);
+		String wasserKraftData = jsonString.substring(jsonString.indexOf(startIndex));
+		List<String> wasserKraftParsedDatasets = new ArrayList<String>();
+		Pattern pattern = Pattern.compile("\\[[^\\[]\\S*?(?=\\]\\})", Pattern.CASE_INSENSITIVE);
 	    Matcher matcher = pattern.matcher(wasserKraftData);
 	    
-	    while (matcher.find()) {
-	    	wasserKraftDatasets.add(matcher.group());
-    	 }
-	    
-	    List<String[]> wasserKraftParsedDatasets = new ArrayList<String[]>();
-	    for (String element : wasserKraftDatasets) {
-	    	wasserKraftParsedDatasets.add(element.split(","));
-	    }
+	    if (matcher.find()) {
+	    	String wasserKraftDatasets = matcher.group(0);
+	    	for (String Dataset : wasserKraftDatasets.split("\\],\\[")) {
+	    		for (String DatasetPart : Dataset.split(",")) {
+		    		wasserKraftParsedDatasets.add(DatasetPart.replaceAll("[\\[\\]]", ""));
+			    }
+		    }
+		}
 	    
 	    System.out.print("Wasserkraft Daten:\n");
-	    for (String[] element : wasserKraftParsedDatasets) {
-	    	//System.out.print("Datum:");
-	        System.out.printf("Datum %s ", Utilities.convertTimestamp(element[0].replace("[", "")));
-	        System.out.printf("Wert %s GW",element[1].replace("]", ""));
-	        System.out.print("\n");
+	    for (int i = 0; i < wasserKraftParsedDatasets.size(); i++) {
+	    	if (i % 2 == 0)
+	    	{
+	    		System.out.printf("Datum %s ", Utilities.convertTimestamp(wasserKraftParsedDatasets.get(i)));
+	    	} 
+	    	else 
+	    	{
+	    		System.out.printf("Wert %s GW",wasserKraftParsedDatasets.get(i));
+	    	}
+	    	
+	    	System.out.print("\n");        
 	    }
 	    
 	    // TODO ... other energy sources
